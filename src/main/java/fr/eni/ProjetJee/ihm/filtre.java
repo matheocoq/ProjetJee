1976507654fd12a41c9c8095c76c434539da3ca4
Matheo COQUEL <matheo.coquel2021@campus-eni.fr>
2022-06-15 14:15:11 +0200

    css accueil

--- src/main/java/fr/eni/ProjetJee/ihm/filtre.java
+++ src/main/java/fr/eni/ProjetJee/ihm/filtre.java
@@ -34,17 +34,17 @@ public class filtre implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		Object seesion = ((HttpServletRequest) request).getSession().getAttribute("utilisateur");
-		if(httpRequest.getServletPath().toLowerCase().contains("style") ||httpRequest.getServletPath().toLowerCase().contains("accueil")|| httpRequest.getServletPath().toLowerCase()=="" || httpRequest.getServletPath().toLowerCase().contains("connexion") || httpRequest.getServletPath().toLowerCase().contains("inscription") ||seesion!=null)
-		{
+		//if(httpRequest.getServletPath().toLowerCase().contains("logoENI.svg") || httpRequest.getServletPath().toLowerCase().contains("style") ||httpRequest.getServletPath().toLowerCase().contains("accueil")|| httpRequest.getServletPath().toLowerCase()=="" || httpRequest.getServletPath().toLowerCase().contains("connexion") || httpRequest.getServletPath().toLowerCase().contains("inscription") ||seesion!=null)
+		//{
 			
 			//Laissons passer la requête vers la ressource qui est autorisée
 			chain.doFilter(request, response);
-		}
+		/*}
 		else
 		{
 			//renvoyons une 403 à l'utilisateur
 			((HttpServletResponse) response).sendRedirect("http://localhost:8080/ProjetJee/Connexion");
-		}
+		}*/
 	}
 
 	
